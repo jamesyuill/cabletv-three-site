931d689050a47065e801d3a1e0cfee890d68e1aa
James Yuill <jamesyuill@jamess-mbp.lan>
2023-07-01 22:51:52 +0100

    adds some gsap animation, but isn't getting triggered more than once

--- main.js
+++ main.js
@@ -7,38 +7,193 @@ import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  50,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
 
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-  antialias: true,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000, 1);
 
-document.body.appendChild(renderer.domElement);
 
-//LIGHTS
 
+
+
+let camera, scene, renderer;
+
+
+
+
+  //Camera
+  camera = new THREE.PerspectiveCamera(50,window.innerWidth / window.innerHeight,0.1,1000);
+  camera.position.set(0, 1.5, 10);
+  camera.rotation.set(0, 0, 0);
+
+  //Scene
+  scene = new THREE.Scene();
+
+  //Renderer
+  renderer = new THREE.WebGLRenderer({canvas,antialias: true,});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x000000, 1);
+  document.body.appendChild(renderer.domElement);
+
+  //Light
 //overhead RectLight
 const rectLightTop = new THREE.RectAreaLight(0xff0000, 10, 5, 1);
 const helperTop = new RectAreaLightHelper(rectLightTop);
 rectLightTop.position.set(0, 4, 0);
 rectLightTop.rotation.set(-(Math.PI / 2), 0, 0);
-rectLightTop.add(helperTop);
+// rectLightTop.add(helperTop);
+scene.add(rectLightTop);
 
 //Left RectLight
 const rectLightBottom = new THREE.RectAreaLight(0x48f7, 10, 3, 1);
 const helperBottom = new RectAreaLightHelper(rectLightBottom);
 rectLightBottom.position.set(0, 0, 0);
 rectLightBottom.rotation.set(Math.PI / 2, 0, 0);
-rectLightBottom.add(helperBottom);
+// rectLightBottom.add(helperBottom);
+scene.add(rectLightBottom);
+
+
+
+//Logo Loader
+
+let logo = new THREE.Group()
+const loader = new TTFLoader();
+const fontLoader = new FontLoader();
+loader.load('./public/fonts/Condition-Regular.ttf', (fnt)=> {
+  let font = fontLoader.parse(fnt)
+    const cableGeometry = new TextGeometry('cable', {font: font,size: 1,height: 0.5,});
+    const tvGeometry = new TextGeometry('tv', {font: font,size: 1,height: 0.5,})
+    const textMaterial = new THREE.MeshPhysicalMaterial({roughness: 0,metalness: 0.3,clearcoat: 0.52,});
+    const cableMesh = new THREE.Mesh(cableGeometry, textMaterial);
+    const tvMesh = new THREE.Mesh(tvGeometry, textMaterial);
+    cableMesh.position.set(0, 0, 0);
+    tvMesh.position.set(1.2, -1.1, 0);
+    const logoGroup = new THREE.Group();
+  logoGroup.add(cableMesh);
+  logoGroup.add(tvMesh);
+    logoGroup.position.set(-1.8, 2, 0.5);
+    logoGroup.rotation.set(-0.3, 0.4, 0);
+
+    logo.add(logoGroup)
+  })
+
+
+scene.add(logo);
+//in case i mess it up
+// loader.load('./public/fonts/Condition-Regular.ttf', function (fnt) {
+//   let font = fontLoader.parse(fnt);
+//   const cableGeometry = new TextGeometry('cable', {font: font,size: 1,height: 0.5,});
+//   const tvGeometry = new TextGeometry('tv', {font: font,size: 1,height: 0.5,});
+//   const textMaterial = new THREE.MeshPhysicalMaterial({roughness: 0,metalness: 0.3,clearcoat: 0.52,});
+//   const cableMesh = new THREE.Mesh(cableGeometry, textMaterial);
+//   const tvMesh = new THREE.Mesh(tvGeometry, textMaterial);
+//   cableMesh.position.set(0, 0, 0);
+//   tvMesh.position.set(1.2, -1.1, 0);
+//   const logoGroup = new THREE.Group();
+//   logoGroup.add(cableMesh);
+//   logoGroup.add(tvMesh);
+//   logoGroup.position.set(-1.8, 2, 0.5);
+//   logoGroup.rotation.set(-0.3, 0.4, 0);
+//   return logoGroup
+
+
+
+
+
+
+
+
+// function generateBox(){
+//   const box2Geo = new THREE.BoxGeometry(1.5,1.5,1.5)
+// const box2Mat = new THREE.MeshStandardMaterial()
+// const box2Mesh = new THREE.Mesh(box2Geo, box2Mat)
+// return box2Mesh;
+// }
+
+// const box2Mesh = generateBox()
+// box2Mesh.position.set(0,0,0)
+// scene.add(box2Mesh)
+
+
+//BOX
+const boxGeo = new THREE.BoxGeometry(1.5,1.5,1.5)
+const boxMat = new THREE.MeshStandardMaterial()
+const boxMesh = new THREE.Mesh(boxGeo, boxMat)
+boxMesh.position.set(-5,2,0.5)
+scene.add(boxMesh)
+
+//ICOSPHERE
+const icoGeo = new THREE.TorusGeometry(1,0.2,100,200)
+const icoMat = new THREE.MeshStandardMaterial()
+const icoMesh = new THREE.Mesh(icoGeo, icoMat)
+icoMesh.position.set(5,2,0.5)
+scene.add(icoMesh);
+
+
+
+
+
+//PLANE
+const planeGeometry = new THREE.PlaneGeometry(15, 10, 100, 100);
+const planeMaterial = new THREE.MeshStandardMaterial();
+const pMaterial = new THREE.PointsMaterial({ size: 0.01, color: 0xffffff });
+const planeMesh = new THREE.Points(planeGeometry, pMaterial);
+planeMesh.rotation.x = -Math.PI / 2;
+planeMesh.position.set(0, -0.5, 0);
+scene.add(planeMesh);
+
+
+
+
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+
+
+
+
+
+
+const count = planeGeometry.attributes.position.count;
+
+function animate() {
+  const now = Date.now() / 300;
+  for (let i = 0; i < count; i++) {
+    const x = planeGeometry.attributes.position.getX(i);
+
+    const xsin = Math.sin(x + now) * Math.random();
+    planeGeometry.attributes.position.setZ(i, xsin);
+  }
+  planeGeometry.attributes.position.needsUpdate = true;
+
+  requestAnimationFrame(animate);
+boxMesh.rotation.x += 0.01
+boxMesh.rotation.y += 0.03
+
+icoMesh.rotation.x += 0.01
+icoMesh.rotation.y += 0.03
+
+  renderer.render(scene, camera);
+}
+
+
+animate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //CAMERA CONTROLS
 //----------------------------------------------------------------
@@ -47,84 +202,17 @@ rectLightBottom.add(helperBottom);
 // controls.dynamicDampingFactor = 0.1;
 // controls.update();
 
-//GEOMETRY
 
-//Box Geo
-const boxGeo = new THREE.BoxGeometry(1.5,1.5,1.5)
-const boxMat = new THREE.MeshStandardMaterial()
-const boxMesh = new THREE.Mesh(boxGeo, boxMat)
-
-scene.add(boxMesh)
-
-
-
-
-
-
-
-
-
-//PLANE
-
-const planeGeometry = new THREE.PlaneGeometry(15, 10, 100, 100);
-const planeMaterial = new THREE.MeshStandardMaterial();
-const pMaterial = new THREE.PointsMaterial({ size: 0.01, color: 0xffffff });
-const planeMesh = new THREE.Points(planeGeometry, pMaterial);
-
-// console.log(pointsArray);
-
-planeMesh.rotation.x = -Math.PI / 2;
-planeMesh.position.set(0, -0.5, 0);
-
-scene.add(planeMesh);
 
 // const size = 15;
 // const divisions = 20;
 // const gridHelper = new THREE.GridHelper(size, divisions);
 // scene.add(gridHelper);
 
-//FONT LOADER
 
-const loader = new TTFLoader();
 
-const fontLoader = new FontLoader();
 
-loader.load('./public/fonts/Condition-Regular.ttf', function (fnt) {
-  let font = fontLoader.parse(fnt);
-  const cableGeometry = new TextGeometry('cable', {
-    font: font,
-    size: 1,
-    height: 0.5,
-  });
-  const tvGeometry = new TextGeometry('tv', {
-    font: font,
-    size: 1,
-    height: 0.5,
-  });
-  const textMaterial = new THREE.MeshPhysicalMaterial({
-    roughness: 0,
-    metalness: 0.3,
-    clearcoat: 0.52,
-  });
-  const cableMesh = new THREE.Mesh(cableGeometry, textMaterial);
-  const tvMesh = new THREE.Mesh(tvGeometry, textMaterial);
-  cableMesh.position.set(0, 0, 0);
-  tvMesh.position.set(1.2, -1.1, 0);
-  logoGroup = new THREE.Group();
-  logoGroup.add(cableMesh);
-  logoGroup.add(tvMesh);
-  logoGroup.position.set(-1.8, 2, 0.5);
-  logoGroup.rotation.set(-0.3, 0.4, 0);
-  
-  scene.add(logoGroup);
-  
-});
 
-//POSITIONALS
-camera.position.set(0, 1.5, 10);
-camera.rotation.set(0, 0, 0);
-
-boxMesh.position.set(-10,2,0.5)
 
 //DAT.GUI
 
@@ -147,51 +235,36 @@ gui.addColor(rectLightBottomParams, 'bottomLight').onChange((value) => {
 
 //ADD TO SCENE
 
-scene.add(rectLightTop, rectLightBottom);
+
 
 //GSAP
+const movableFeast = new THREE.Group()
+movableFeast.add(boxMesh, logo, icoMesh)
+scene.add(movableFeast);
+const boxAnim = gsap.timeline({paused:true})
+.to(movableFeast.position, {x:5,duration:2})
 
+const boxAnim2 = gsap.timeline({paused:true})
+.to(movableFeast.position, {x:0,duration:2})
 
-let boxAnim = gsap
-    .to(boxMesh.position, {x:0, duration:1, paused:true})
-    .to(logoGroup.position, {x:5,duration:1, paused:true})
-
-
+const boxAnim3 = gsap.timeline({paused:true})
+.to(movableFeast.position, {x:-5,duration:2})
 
 const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+const button3 = document.getElementById('button3')
 button1.addEventListener('mouseenter', ()=>{
   boxAnim.play();
+  
+})
+button2.addEventListener('mouseenter', ()=>{
+  boxAnim2.play();
+
+})
+button3.addEventListener('mouseenter', ()=>{
+  boxAnim3.play();
 })
 
 
 
 
-//ANIMATE
-const count = planeGeometry.attributes.position.count;
-
-function animate() {
-  const now = Date.now() / 300;
-  for (let i = 0; i < count; i++) {
-    const x = planeGeometry.attributes.position.getX(i);
-
-    const xsin = Math.sin(x + now) * Math.random();
-    planeGeometry.attributes.position.setZ(i, xsin);
-  }
-  planeGeometry.attributes.position.needsUpdate = true;
-
-  requestAnimationFrame(animate);
-boxMesh.rotation.x += 0.01
-boxMesh.rotation.y += 0.03
-
-  renderer.render(scene, camera);
-}
-animate();
-
-window.addEventListener('resize', onWindowResize, false);
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
