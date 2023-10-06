af18d43173ece63f1ea49e4b10b359e1614325e3
James Yuill <jamesyuill@jamess-mbp.lan>
2023-10-06 11:17:13 +0100

    change in file linking

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three/build/three.module.js';
 import { gsap } from 'gsap';
 import { GUI } from 'dat.gui';
 import { TTFLoader } from 'three/addons/loaders/TTFLoader.js';
